1d9e238b4a9c30039d2e66e1f5354d63ade258a4
r128 <insomnia128@hotmail.com>
2015-01-16 01:09:47 +0800

    Added SSO Login functionality

--- app/controllers/UserMgr.java
+++ app/controllers/UserMgr.java
@@ -6,7 +6,12 @@ import models.user.User;
 import play.data.DynamicForm;
 import play.data.Form;
 import play.mvc.*;
-import sun.misc.BASE64Encoder;
+import javax.crypto.*;
+import org.apache.commons.codec.binary.Base64;
+import java.net.URLEncoder;
+import javax.crypto.spec.SecretKeySpec;
+import java.security.*;
+
 
 import java.security.SecureRandom;
 import java.util.Date;
@@ -20,33 +25,66 @@ public class UserMgr extends Controller{
     //TODO: refactor to a security class
     public static Result loginUser() {
         DynamicForm requestData = Form.form().bindFromRequest();
-        String username = requestData.get("username");
-        String password = requestData.get("password");
 
-        User u = Ebean.find(User.class)
-                .where()
-                .eq("username", username)
-                .findUnique();
+        final String[] keys = {
+                "oauth_callback",
+                "oauth_consumer_key",
+                "oauth_nonce",
+                "oauth_signature_method",
+                "oauth_timestamp",
+                "oauth_version",
+                "smu_domain",
+                "smu_fullname",
+                "smu_groups",
+                "smu_username"
+        };
 
-        boolean isAuth = false;
+        final String SECRET_KEY = "1234567890";
 
-        if (u == null) {
-            return status(419, "User not found!");
-        } else{
-            try {
-                String userProvidedPass = SecurityUtility.getHash(password, u.salt);
-                isAuth = userProvidedPass.equals(u.password);
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-            if (isAuth) {
-                session().put("username", username);
-                return ok("<form action=\"logout\" method=\"POST\"> <input type=\"submit\" value=\"Logout!\"></form>").as("text/html");
-            } else {
-                return status(420, "Wrong password!");
-            }
+        String callbackUrl = "http://localhost:9000/user/login";
+
+        String uri = "POST&" + encode(callbackUrl) + "&" ;
+
+        String pairs = "";
+        for (int i = 0 ; i < keys.length-1 ; i++) {
+            pairs += keys[i] + "=" + encode(requestData.get(keys[i])) + "&";
         }
 
+        pairs += keys[keys.length - 1] + "=" + encode(requestData.get(keys[keys.length - 1]));
+
+        uri += encode(pairs);
+
+
+        try {
+            Mac mac = Mac.getInstance("HmacSHA1");
+            SecretKey secretKey = new SecretKeySpec( (SECRET_KEY + "&").getBytes(), mac.getAlgorithm());
+
+            try {
+                mac.init(secretKey);
+
+                String generatedSignature = Base64.encodeBase64String(mac.doFinal(uri.getBytes())).trim();
+                String serverSignature = (requestData.get("oauth_signature"));
+
+                long serverSignatureTime = Long.parseLong(requestData.get("oauth_timestamp"));
+                //requestData.get("oauth_timestamp")
+                long currentTime = System.currentTimeMillis() / 1000;
+
+
+                if (serverSignature.equals(generatedSignature) && (Math.abs(serverSignatureTime - currentTime) <= 30)) {
+                    session().put("username", requestData.get("smu_username"));
+
+                    return ok("<form action=\"logout\" method=\"POST\">"+requestData.get("smu_username")+ "<input type=\"submit\" value=\"Logout!\"></form>").as("text/html");
+                }
+
+            }catch (InvalidKeyException e) {
+                e.printStackTrace();
+            }
+
+        } catch(NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+
+        return ok("Login failed");
     }
 
     public static Result logoutUser() {
@@ -83,6 +121,19 @@ public class UserMgr extends Controller{
 
 
         return status(503, "Something went wrong!");
+    }
+
+    public static String encode(String plain) {
+        try {
+            String encoded = URLEncoder.encode(plain, "UTF-8");
+
+            return encoded.replace("*", "%2A")
+                    .replace("+", "%20")
+                    .replace("%7E", "~");
+        } catch (Exception e) {
+            e.printStackTrace(); // hopefully this wont happen
+        }
+        return "";
     }
 
 }
